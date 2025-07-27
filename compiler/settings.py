56e9a06fc647503b5fb2465065a49dac11b6497e
Akash Raj <iamakashraj51@gmail.com>
2025-07-28 02:54:53 +0530

    updated views.py

--- compiler/settings.py
+++ compiler/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+USE_TZ = True
+TIME_ZONE = 'Asia/Kolkata'  # For IST (UTC+5:30)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
